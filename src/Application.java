ca1320759d4da55b62fe6138f5e7e89713116cc3
tuan-nguye <tuananhn67@yahoo.com>
2022-03-14 15:49:15 +0100

    dfs, iterate over all files in source

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,10 @@
 public class Application {
     public static void main(String[] args) {
-
+        Organizer org = new DefaultOrganizer();
+        try {
+            org.copyAndOrganize(args[0], args[1]);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/DefaultOrganizer.java
+++ src/DefaultOrganizer.java
@@ -1,6 +1,23 @@
+import java.io.File;
+
 public class DefaultOrganizer implements Organizer {
     @Override
     public void copyAndOrganize(String source, String destination) {
+        System.out.printf("source = %s, dest = %s\n", source, destination);
+        File root = new File(source);
+        if(!root.isDirectory() || !(new File(destination)).isDirectory())
+            throw new IllegalArgumentException("source is not a directory");
 
+        dfs(root);
+    }
+
+    private void dfs(File dir) {
+        for(File f : dir.listFiles()) {
+            if(f.isDirectory()) {
+                dfs(f);
+            } else {
+                System.out.println(f.getName());
+            }
+        }
     }
 }
