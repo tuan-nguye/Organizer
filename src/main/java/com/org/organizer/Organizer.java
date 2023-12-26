fb040b713809e970f1003235d6dccf175c6fd527
tuan-nguye <70942348+tuan-nguye@users.noreply.github.com>
2023-12-26 16:30:35 +0100

    added comments to the rest of organizer package

--- src/main/java/com/org/organizer/Organizer.java
+++ src/main/java/com/org/organizer/Organizer.java
@@ -14,53 +14,109 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public abstract class Organizer implements Subject<Integer> {
-    private List<Observer> obs = new ArrayList<>();
-    private int count = 0;
-    protected ICopy operation;
-    protected FileGraphOperation fileGraphOperation;
 
+/**
+ * This abstract class offers abstract methods that should be implemented in its
+ * subclasses for organizing files. Also it implements the Subject interface
+ * to count how many files have been organized if an observer needs monitoring.
+ * The destination, in which the files are organized, is set during object creation.
+ */
+public abstract class Organizer implements Subject<Integer> {
+    // list of observers that are to be updated
+    private List<Observer> obs = new ArrayList<>();
+    // count how many files have been organized in the current operation
+    private int count = 0;
+    // template class of the copy/move operation
+    protected ICopy operation;
+    // filegraphoperation used for executing complex file operations on the filegraph
+    protected FileGraphOperation fileGraphOperation;
+    // stores all file extensions that are allowed, all others are ignored
     private Set<String> allowedFileExtensions = null;
 
+    /**
+     * Organizer class constructor.
+     * @param operation copy/move operation to be used when organizing
+     * @param repoPath organizer repository path, destination path of the organizer
+     */
     public Organizer(ICopy operation, String repoPath) {
         this.operation = operation;
         fileGraphOperation = new FileGraphOperation(FileGraphFactory.get(repoPath));
     }
 
+    /**
+     * Copy and organizer all files in the 'source' directory and copy/move them into
+     * the repository path as its destination. The method searches through all files
+     * recursively and only allows files with valid file extensions.
+     * @param source source directory as string path
+     */
     public abstract void copyAndOrganize(String source);
 
+    /**
+     * Increment the counter. This function should be called after successfully organizing
+     * a file.
+     */
     public void incrementCounter() {
         count++;
     }
 
+    /**
+     * Add a file extension to the set of allowed extensions
+     * @param ext file extension as string
+     */
     public void allowFileExtension(String ext) {
         if(allowedFileExtensions == null) allowedFileExtensions = new HashSet<>();
         allowedFileExtensions.add(ext);
     }
 
+    /**
+     * This function returns whether a file extension is allowed or not. If no file extensions
+     * have been set as allowed, then allow all files.
+     * @param ext file extension as string
+     * @return true if the extension is allowed, false if not
+     */
     public boolean fileExtensionAllowed(String ext) {
         if(allowedFileExtensions == null) return true;
         else return allowedFileExtensions.contains(ext);
     }
 
+    /**
+     * get the current count of processed/organized files.
+     * @return
+     */
     public int getCount() {
         return this.count;
     }
 
+    /**
+     * register an observer
+     * @param o observer object
+     */
     public void register(Observer o) {
         obs.add(o);
     }
 
+    /**
+     * unregister an observer
+     * @param o observer object
+     */
     public void unregister(Observer o) {
         obs.remove(o);
     }
 
+    /**
+     * Notify all observers by calling .update() on all registered observers.
+     */
     public void notifyObservers() {
         for(Observer o : obs) {
             o.update();
         }
     }
 
+    /**
+     * get the organizer's state by returning how many files have been processed
+     * by its current copyAndOrganize() method.
+     * @return
+     */
     public Integer getState() {
         return count;
     }
--- src/main/java/com/org/organizer/ThresholdOrganizer.java
+++ src/main/java/com/org/organizer/ThresholdOrganizer.java
@@ -13,21 +13,46 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.Locale;
 
-public class ThresholdOrganizer extends Organizer {
-    private int threshold;
-    private ICopy move = new Move();
 
+/**
+ * This organizer implementation organizes files by date and time, in the order
+ * year -> month -> day of month -> hour -> minute -> second. A threshold limits
+ * all folders to a maximum size. If a folder exceeds the limit then all files are copied
+ * into newly created subdirectories. Files are only stored in the leaf folders and not
+ * in any inner folders.
+ */
+public class ThresholdOrganizer extends Organizer {
+    // file count threshold for folders
+    private int threshold;
+
+    /**
+     * ThresholdOrganizer constructor
+     * @param op copy or move operation to use on the files
+     * @param threshold maximum number of files in each folder
+     * @param repoPath repository path for the organizer, destination path
+     */
     public ThresholdOrganizer(ICopy op, int threshold, String repoPath) {
         super(op, repoPath);
         this.threshold = threshold;
     }
 
+    /**
+     * Iterate through all files in the source directory recursively. Check the file's
+     * extension. If it's allowed then look at the file's datetime attribute and search
+     * for the correct location to copy/move it to. After copying/moving update its state
+     * and notify all observers of the current progress.
+     * @param source source directory as string path
+     */
     @Override
     public void copyAndOrganize(String source) {
         dfs(new File(source));
     }
 
-
+    /**
+     * This function iterates through file objects recursively and is the implementation
+     * of the ThresholdOrganizer's copyAndOrganize function.
+     * @param file
+     */
     private void dfs(File file) {
         if(file.isFile()) {
             if(!fileExtensionAllowed(FileTools.getFileExtension(file))) return;
@@ -41,6 +66,12 @@ public class ThresholdOrganizer extends Organizer {
         }
     }
 
+    /**
+     * Copy or move a file to it's correct location. If a folder exceeds its size threshold
+     * then reorganize all files in that folder.
+     * @param f
+     * @return
+     */
     protected boolean copyFile(File f) {
         FileGraph.Node node = fileGraphOperation.copyFile(operation, f);
         if(node == null) return false;
