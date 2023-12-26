41ccd018fd51d920b6c5ff01d36370ae61aeb9f9
tuan-nguye <70942348+tuan-nguye@users.noreply.github.com>
2023-12-26 16:06:05 +0100

    added comments to observer package

--- src/main/java/com/org/observer/Observer.java
+++ src/main/java/com/org/observer/Observer.java
@@ -1,5 +1,14 @@
 package com.org.observer;
 
+
+/**
+ * Observer interface with a pull implementation. Part of the Subject-Observer
+ * design pattern.
+ */
 public interface Observer {
+    /**
+     * update the observer by pulling the changed values
+     * from the Subject.
+     */
     void update();
 }
--- src/main/java/com/org/observer/Subject.java
+++ src/main/java/com/org/observer/Subject.java
@@ -1,8 +1,33 @@
 package com.org.observer;
 
+
+/**
+ * Subject interface. Part of the Subject-Observer design pattern.
+ *
+ * @param <T>
+ */
 public interface Subject<T> {
+    /**
+     * Register a new observer object
+     * @param o
+     */
     void register(Observer o);
+
+    /**
+     * Unregister an existing observer object
+     * @param o
+     */
     void unregister(Observer o);
+
+    /**
+     * When the subject was updated, then notify all registered observers.
+     */
     void notifyObservers();
+
+    /**
+     * Get the state of the object. This function is used by the observers
+     * to pull the updates from the subject.
+     * @return the subject's current state
+     */
     T getState();
 }
